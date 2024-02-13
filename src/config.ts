138dafc860257b42b7fe8207fdc3fcb1cdcfb1ec
yigal <yigal@THC6>
2024-02-13 21:30:51 +0200

    remove: text-transform: capitalize

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,25 @@
 export const AppConfig = {
     baseUrl: 'https://localhost:44343/api',
-    baseImg:'http://134.209.122.168/',
-    baseImgUrl:'https://http://localhost:4200/',
+    //    baseUrl: 'https://www.gogis.co.il/api',
+    baseImg: 'http://134.209.122.168/',
+
+    baseImgUrl: 'http://localhost:4200/',
+    //    baseImgUrl:'https://www.gogis.co.il/website/',
+
     login: '/user/login',
-    
+
     cards: '/slider',
-    teamCards:'/team',
+    teamCards: '/team',
     contactUs: '/contact_us',
-    
-    weHere:'/we_are_here',
-    events:'/upcoming_events',
+
+    weHere: '/we_are_here',
+    events: '/upcoming_events',
 
     synopsis: '/synopsis',
 
-    deleteEventData:'/upcoming_events',
-    list:'/register_events' ,
-    createEvent:'/upcoming_events'   
+    deleteEventData: '/upcoming_events',
+    list: '/register_events',
+    createEvent: '/upcoming_events'
 }
 
 export const ConfigDetails = {
